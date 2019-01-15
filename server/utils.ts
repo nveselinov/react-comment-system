75838ae5046f7c25ba31ca4cb86b31434c3fcf65
Nasi Atanasov <nasi.atansov@gmail.com>
2019-01-15 15:52:00 +0200

    react-script version bump and minor refactorings

--- server/utils.ts
+++ server/utils.ts
@@ -1,6 +1,8 @@
-export function getLastCommentId(array: Array<{ commentId: number }>): number {
+export const getLastCommentId = (
+  array: Array<{ commentId: number }>
+): number => {
   if (array.length > 0) {
     return array[array.length - 1].commentId;
   }
   return 0;
-}
+};
